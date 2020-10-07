b9bd54f7097e8129ac95fa5e767fea10c9865393
Osenias Oliveira <oseniasjunior@gmail.com>
2020-10-06 20:56:24 -0400

    Ajustes nos modelos

--- core/models.py
+++ core/models.py
@@ -3,12 +3,35 @@ from django.db import models
 
 # Create your models here.
 
-class Department(models.Model):
+class ModelBase(models.Model):
     id = models.AutoField(
         db_column='id',
         null=False,
         primary_key=True
     )
+    created_at = models.DateTimeField(
+        db_column='created_at',
+        null=False,
+        auto_now_add=True
+    )
+    modifed_at = models.DateTimeField(
+        db_column='modified_at',
+        null=False,
+        auto_now=True
+    )
+    active = models.BooleanField(
+        db_column='active',
+        null=False,
+        default=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+# id, created_at, modified_at, active
+
+class Department(ModelBase):
     name = models.CharField(
         db_column='name',
         null=False,
@@ -20,12 +43,7 @@ class Department(models.Model):
         managed = True
 
 
-class Employee(models.Model):
-    id = models.AutoField(
-        db_column='id',
-        null=False,
-        primary_key=True
-    )
+class Employee(ModelBase):
     name = models.CharField(
         db_column='name',
         null=False,
@@ -40,4 +58,67 @@ class Employee(models.Model):
 
     class Meta:
         db_table = 'employee'
+        managed = True
+
+
+class Category(ModelBase):
+    name = models.CharField(
+        db_column='name',
+        null=False,
+        max_length=104
+    )
+    comission_percentage = models.DecimalField(
+        db_column='comission_percentage',
+        null=False,
+        max_digits=10,
+        decimal_places=2
+    )
+    products = models.ManyToManyField(
+        to='Product',
+        through='ProductCategory'
+    )
+
+    class Meta:
+        db_table = 'category'
+        managed = True
+
+
+class Product(ModelBase):
+    name = models.CharField(
+        db_column='name',
+        null=False,
+        max_length=104
+    )
+    price = models.DecimalField(
+        db_column='price',
+        null=False,
+        max_digits=10,
+        decimal_places=2
+    )
+    categories = models.ManyToManyField(
+        to='Category',
+        through='ProductCategory'
+    )
+
+    class Meta:
+        db_table = 'product'
+        managed = True
+
+
+class ProductCategory(ModelBase):
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.DO_NOTHING,
+        db_column='id_category',
+        null=False
+    )
+    product = models.ForeignKey(
+        to='Product',
+        on_delete=models.DO_NOTHING,
+        db_column='id_product',
+        null=False
+    )
+
+    class Meta:
+        db_table = 'product_category'
         managed = True
