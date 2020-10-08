619482f86a4eec47ae344470fe7e165f56881867
Osenias Oliveira <oseniasjunior@gmail.com>
2020-10-07 20:27:51 -0400

    Add queries

--- core/queries.py
+++ core/queries.py
@@ -1,4 +1,5 @@
 from core import models
+from django.db.models import Q, Case, When, CharField, Value
 
 
 def get_all_products():
@@ -17,4 +18,20 @@ def get_all_products_limit_columns():
 def get_product_by_id(name):
     queryset = models.Product.objects.filter(name__icontains=name, active=True)
     print(queryset.query)
+    return queryset
+
+
+def get_product_by_id_with_or(name):
+    queryset = models.Product.objects.filter(created_at__month=2020)
+    print(queryset.query)
+    return queryset
+
+
+def get_custom_query():
+    queryset = models.Product.objects.annotate(
+        status=Case(
+            When(price__gte=3000, then=Value('Caro', output_field=CharField())),
+            default=Value('Normal', output_field=CharField())
+        )
+    ).values('id', 'name', 'status')
     return queryset
