de6a51dc9e1a8403b7b7e412e6bdba03d88af6ec
Osenias Oliveira <oseniasjunior@gmail.com>
2020-10-07 20:27:31 -0400

    Adicionando os admins

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
+
+from core import models
+
 
 # Register your models here.
+
+@admin.register(models.Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'active', 'modified_at']
+    list_display_links = ['id', 'name', 'active', 'modified_at']
+    search_fields = ['name']
+    list_per_page = 10
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'active', 'modified_at']
+    list_display_links = ['id', 'name', 'active', 'modified_at']
+    search_fields = ['name']
+    list_per_page = 10
+
+
+class CategoryInLine(TabularInline):
+    model = models.ProductCategory
+    extra = 1
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'active', 'modified_at']
+    list_display_links = ['id', 'name', 'price', 'active', 'modified_at']
+    search_fields = ['name']
+    list_per_page = 10
+    inlines = [CategoryInLine]
